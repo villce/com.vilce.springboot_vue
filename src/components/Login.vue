f00705ebcb33c53f836c4b4dd5c31a761f30490a
leicaizhe <caizhe@eastmoney.com>
2019-12-20 17:57:48 +0800

    登录Demo

---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
+    <br><br>
+    密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
+    <br><br>
+    <button v-on:click="login">登录</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      responseResult: []
+    }
+  },
+  methods: {
+    login () {
+      this.$axios
+        .post('/user/test', {
+          username: this.loginForm.username,
+          password: this.loginForm.password
+        })
+        .then(successResponse => {
+          if (successResponse.data.data === true) {
+            this.$router.replace({path: '/index'})
+          }
+        })
+        .catch(failResponse => {
+        })
+    }
+  }
+}
+</script>
